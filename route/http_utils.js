3627e3462c4bf2f7a43eabc4bf8762fde561660a
Taka <wang.chia.ming@gmail.com>
2013-08-20 17:21:02 +0800

    Update http_utils.js

--- route/http_utils.js
+++ route/http_utils.js
@@ -1,13 +1,13 @@
-var redirect = function(req, res, _url){
+var redirect = function(req, res, _url) { //help to redirect
         var url = "http://" + req.headers.host  + _url;
         res.statusCode = 302;
         res.setHeader('Content-Type', 'text/plain');
         res.setHeader('Location', url);
         res.end('Redirecting to ' + url);
     },
-    writeHtml = function(res, str) {
+    writeHtml = function(res, str) { //help to response simple html
         res.setHeader('Content-Type', 'text/html');
-        res.end(str);
+        res.end(str);
     };
 
 module.exports.redirect  = redirect;
